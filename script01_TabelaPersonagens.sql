83d0d567b36c45e64020858d74ca60b85ea22e3b
Roberto Dias <robertsouaz@hotmail.com>
2025-04-23 08:19:11 -0300

    Update script01_TabelaPersonagens.sql

--- script01_TabelaPersonagens.sql
+++ script01_TabelaPersonagens.sql
@@ -1,13 +1,3 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
-
 BEGIN TRANSACTION;
 CREATE TABLE [TB_PERSONAGENS] (
     [Id] int NOT NULL IDENTITY,
